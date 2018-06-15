aa19f2a8cad54112d6a88e217e337bd202725089
ctechhindi <ctechhindi@gmail.com>
2018-06-15 09:07:06 +0530

    Send Mail with PHPMailer Library

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,14 +1,22 @@
 <?php
-
-
 namespace App\Controller;
 
 class PagesController extends AppController
 {
+    public $components = array("Email");
 
     public function index()
     {
-        echo "Home Page";
+        $to = 'jeevan15498@gmail.com';
+        $subject = 'Hi buddy, i got a message for you.';
+        $message = 'Nothing much. Just test out my Email Component using PHPMailer.';
+        
+        try {
+            $mail = $this->Email->send_mail($to, $subject, $message);
+            print_r($mail);
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
         exit;
     }
 }
